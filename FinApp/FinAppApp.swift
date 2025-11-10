b08ea5c96d5a9e2d837726b0d97d6bdebf879457
Finbar Tracey <Finbartracey@gmail.com>
2025-11-10 21:54:25 +0000

    Initial Commit

---
+++ FinApp/FinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinAppApp.swift
+//  FinApp
+//
+//  Created by Finbar Tracey on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
